f82ac1b8d03bf56f1635fb1d5e8b2e0165d7d701
etigoshun <k021g1375@g.neec.ac.jp>
2022-11-26 17:52:05 +0900

    2_03完了

--- Object3d.h
+++ Object3d.h
@@ -37,12 +37,12 @@ public: // サブクラス
 	};
 
 private: // 定数
-	static const int division = 50;					// 分割数
+	static const int division = 50;			// 分割数
 	static const float radius;				// 底面の半径
 	static const float prizmHeight;			// 柱の高さ
 	static const int planeCount = division * 2 + division * 2;		// 面の数
-	static const int vertexCount = 4;		// 頂点数
-	static const int indexCount = 3 * 2;	//インデックス数
+	static const int vertexCount = 1;		// 頂点数
+	//static const int indexCount = 3 * 2;	//インデックス数
 
 
 public: // 静的メンバ関数
@@ -117,7 +117,7 @@ private: // 静的メンバ変数
 	// 頂点バッファ
 	static ComPtr<ID3D12Resource> vertBuff;
 	// インデックスバッファ
-	static ComPtr<ID3D12Resource> indexBuff;
+	//static ComPtr<ID3D12Resource> indexBuff;
 	// テクスチャバッファ
 	static ComPtr<ID3D12Resource> texbuff;
 	// シェーダリソースビューのハンドル(CPU)
@@ -137,11 +137,11 @@ private: // 静的メンバ変数
 	// 頂点バッファビュー
 	static D3D12_VERTEX_BUFFER_VIEW vbView;
 	// インデックスバッファビュー
-	static D3D12_INDEX_BUFFER_VIEW ibView;
+	//static D3D12_INDEX_BUFFER_VIEW ibView;
 	// 頂点データ配列
 	static VertexPosNormalUv vertices[vertexCount];
 	// 頂点インデックス配列
-	static unsigned short indices[indexCount];
+	//static unsigned short indices[indexCount];
 	//ビルボード行列
 	static XMMATRIX matBillboard;
 	//Y軸回りビルボード行列
--- Resources/Shaders/BasicGeometryShader.hlsl
+++ Resources/Shaders/BasicGeometryShader.hlsl
@@ -1,17 +1,23 @@
 #include "BasicShaderHeader.hlsli"
-
+//三角形の入力から、三角形を2つ入力
 [maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION, 
+	point VSOutput input[1] : SV_POSITION,
+	//点ストリーム
 	inout TriangleStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
-	}
+	GSOutput element;
+	//共通	
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	//1点目
+	element.svpos = input[0].svpos;
+	output.Append(element);
+	//2点目
+	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
+	output.Append(element);
+	//3点目
+	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(element);
 }
